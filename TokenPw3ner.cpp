2f155a1146de21e13f1c412bea410a2dc67ecdf4
Ali Bin Mohammad <43062310+BINM7MD@users.noreply.github.com>
2022-08-24 22:56:16 +0300

    Delete TokenPw3ner.cpp

--- TokenPw3ner.cpp
+++
@@ -1,9 +0,0 @@
-// TokenPw3ner.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
